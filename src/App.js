c19917c6a8e3bf4e4b3dd91bd149560ce9fe88ff
AunAli7 <aunali007812@gmail.com>
2025-01-31 17:01:31 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './Home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;
